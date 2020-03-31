be973fff06c4a03aeb5cb4012900ddbf1f534207
Marina Poprotskaya <mitsevich.marina@gmail.com>
2020-03-31 19:01:51 +0300

    refactor: renew script file`s structure

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-// Создаю макет клавиатуры
+
+const keyboardButtonsRU = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift', 'Ctrl', 'Fn', 'Alt', 'Space', 'Alt', 'Ctrl', '←', '↓', '→'];
+
+const keyCodes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'WakeUp', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+
+// Создаю поле для ввода
 const textareaField = document.createElement('textarea');
 document.body.append(textareaField);
 textareaField.classList.add('textarea-class');
-textareaField.setAttribute('autofocus', ''); // Не работает
+textareaField.focus();
 
-
+// Создаю каркас клавиатуры
 const keyboardField = document.createElement('ul');
 document.body.append(keyboardField);
 keyboardField.classList.add('keyboard-class');
 
-const keyboardButtons = [
-  [
-    'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'CapsLK', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&uarr;', 'Shift', 'Ctrl', 'Fn', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'],
-  [
-    'Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '|', 'CapsLK', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '&uarr;', 'Shift', 'Ctrl', 'Fn', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'],
-  [
-    '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'CapsLK', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&uarr;', 'Shift', 'Ctrl', 'Fn', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'],
-    [
-      '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '|', 'CapsLK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&uarr;', 'Shift', 'Ctrl', 'Fn', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'],
-];
+// Создаю кнопки клавиатуру
+function makeKeyboard() {
+  for (let i = 0; i < keyboardButtonsRU.length; i += 1) {
+    const buttons = document.createElement('li');
 
+    buttons.classList.add('btn-class');
+    buttons.innerHTML = keyboardButtonsRU[i];
 
-for (let i = 0; i < keyboardButtons[0].length; i += 1) {
-  const buttons = document.createElement('li');
+    buttons.setAttribute('id', keyCodes[i]);
 
-  buttons.classList.add('btn-class');
-  buttons.innerHTML = keyboardButtons[0][i];
-
-  buttons.setAttribute('id', keyboardButtons[0][i].charCodeAt(0));
-
-  keyboardField.append(buttons);
-  if (keyboardButtons[0][i] === 'Backspace' || keyboardButtons[0][i] === 'Tab' || keyboardButtons[0][i] === 'CapsLK' || keyboardButtons[0][i] === 'Enter' || keyboardButtons[0][i] === 'Shift' || keyboardButtons[0][i] === 'Ctrl' || keyboardButtons[0][i] === 'Fn' || keyboardButtons[0][i] === 'Alt' || keyboardButtons[0][i] === 'Space') {
-    buttons.classList.add(`${keyboardButtons[0][i]}-class`);
+    keyboardField.append(buttons);
   }
 }
+makeKeyboard();
 
 // Делаю интерактивность кнопок
 keyboardField.addEventListener('mousedown', (event) => {
@@ -52,33 +46,66 @@ keyboardField.addEventListener('mouseup', (event) => {
 });
 
 // Выводим буквы на экран
-textareaField.value = '';
-keyboardField.addEventListener('click', (event) => {
-  if (event.target.tagName !== 'LI') return;
-  const text = event.target.innerHTML;
-  if (text === 'Space') {
-    (textareaField.value += ' ');
-  } else if (text === 'Backspace') {
-    textareaField.value = textareaField.value.substr(0, textareaField.value.length - 1);
-  } else if (text === 'Enter') {
-    textareaField.value += '\r\n';
-  } else if (text === 'Tab') {
-    textareaField.value += '    ';
-  } else if (text === 'CapsLK') {
+function showLetters() {
+  textareaField.value = '';
+  keyboardField.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'LI') return;
+    const text = event.target.innerHTML;
+    if (text === 'Space') {
+      (textareaField.value += ' ');
+    } else if (text === '←' || text === '↑' || text === '→' || text === '↓') {
+      textareaField.value += '';
+    } else if (text === 'Backspace') {
+      textareaField.value = textareaField.value.substr(0, textareaField.value.length - 1);
+    } else if (text === 'Enter') {
+      textareaField.value += '\r\n';
+    } else if (text === 'Tab') {
+      textareaField.value += '    ';
+    } else if (text === 'CapsLock') {
+      document.querySelectorAll('li').forEach((el) => {
+        if (el.innerHTML.length === 1) {
+          el.classList.toggle('upper');
+        }
+      });
+    } else if (text.length === 1) {
+      textareaField.value += text;
+    }
+    textareaField.focus();
+  });
+}
+showLetters();
+
+// Связь реальной клавиатуры с виртуальной
+function showButton() {
+  window.addEventListener('keydown', (event) => {
+    const li = document.getElementById(event.code);
+    if (li != null) {
+      Array.from(document.getElementsByTagName('li')).forEach((el) => {
+        if (el.classList.contains('press-btn') && el.innerHTML === 'Tab') { el.classList.remove('press-btn'); }
+      });
+      li.classList.add('press-btn');
+    }
+  });
+  window.addEventListener('keyup', (event) => {
+    const li = document.getElementById(event.code);
+    if (li != null) {
+      li.classList.remove('press-btn');
+    }
+  });
+}
+showButton();
+
+// Для CapsLock
+window.addEventListener('keydown', (event) => {
+  if (event.code === 'CapsLock') {
     document.querySelectorAll('li').forEach((el) => {
       if (el.innerHTML.length === 1) {
         el.classList.toggle('upper');
       }
     });
-  } else if (text.length === 1) {
-    textareaField.value += text;
   }
 });
 
-// window.addEventListener('keydown', (event) => {
-//   const li = keyboardField.getElementById(event.keyCode);
-
-//   if (li != null) {
-
-//   }
-// });
+window.addEventListener('keydown', (event) => {
+  console.log(event);
+});
